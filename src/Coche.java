e2af3f04d3b769c025b3e696e6f8b26c75524940
afermar860n <afermar860n@g.educaand.es>
2023-11-08 10:04:45 +0100

    UD3

---
+++ src/Coche.java
@@ -0,0 +1,28 @@
+public class Coche {
+    String marca;
+    String modelo;
+    String matricula;
+    String color;
+
+    int numPuertas;
+    String tipoMotor;
+    String obtenerDescripcion(){
+        return "Coche" + marca + " " + modelo + "con matricula " + matricula + " y de color " + color;
+
+    }
+    void arrancarMotor(){
+        //TODO:codigo
+    }
+
+    void pararmotor(){
+        //codigo
+    }
+    void girarIzquierda(){
+        //codigo
+    }
+
+    void girarDerecha(){
+        //codigo
+    }
+
+}
---
+++ src/Prueba.java
@@ -0,0 +1,10 @@
+public class Prueba {
+    public static void main(String[] args) {
+        Rectangulo r = new Rectangulo();
+        r.base = 10;
+        r.altura = 5;
+        System.out.println("El rectángulo tiene: " +
+                "base = " + r.base + " y altura = " + r.altura);
+
+    }
+}
